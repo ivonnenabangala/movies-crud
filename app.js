7084ac48edd97579a0758b08cd50e7df880d17e7
ivonnenabangala <ivonnenabangala@gmail.com>
2025-01-15 17:19:17 +0300

    work on update movie

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const getMovie = () => {
         <span class="fa fa-star"></span>
         <span class="fa fa-star"></span>
         <div class="buttons">
-        <button>Edit</button>
+        <button onclick="openForm(); prepopulateForm(${movie.id})">Edit</button>
         <button onclick="deleteMovie(${movie.id})">Delete</button>
         </div>
     `
@@ -68,3 +68,59 @@ const deleteMovie = (id) => {
       getMovie();
       console.log(movies);
 }
+
+const prepopulateForm = (id) => {
+    const movie = movies.find((movie) => movie.id === id); 
+    if (movie) {
+        const editFormContainer = document.getElementById("editForm");
+        editFormContainer.innerHTML = ""; 
+
+        const movieDetails = document.createElement("div");
+        movieDetails.className = "form-popup";
+        movieDetails.style.display = "block"; 
+        movieDetails.innerHTML = `
+        <form action="submit" class="form-container">
+            <h1>Edit Movie</h1>
+
+            <label for="editImageUrl">Image URL</label><br>
+            <input type="text" id="editImageUrl" value="${movie.image}"><br>
+
+            <label for="editDescription">Description</label><br>
+            <input type="text" id="editDescription" value="${movie.description}"><br>
+
+            <label for="editTitle">Title</label><br>
+            <input type="text" id="editTitle" value="${movie.title}"><br>
+
+            <label for="editRating">Rating</label><br>
+            <input type="text" id="editRating" value="${movie.rating}"><br>
+
+            <button type="button" class="btn" onclick="editMovie(${movie.id})">Save</button>
+            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+        </form>
+        `;
+
+        editFormContainer.appendChild(movieDetails); 
+    }
+};
+
+const editMovie = (id) => {
+    const movie = movies.find((movie) => movie.id === id)
+    if(movie){
+        movie.image = document.getElementById("editImageUrl").value
+        movie.description = document.getElementById("editDescription").value
+        movie.title = document.getElementById("editTitle").value
+        movie.rating = document.getElementById("editRating").value
+        closeForm(); 
+        getMovie();
+    }
+
+}
+const openForm = () => {
+    document.getElementById("editForm").style.display = "block";
+}
+
+  
+  const closeForm = () => {
+    document.getElementById("editForm").style.display = "none";
+
+  }
